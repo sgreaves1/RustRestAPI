1b370503dd73937eddc981b1c387328a66cf2ba8
Sam Greaves <samgreaves0@gmail.com>
2019-01-30 12:49:09 +0000

    updated the durability endpoint to require an item name and an attacking item name

--- app/routes/durability.js
+++ app/routes/durability.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 const HttpStatus = require('literal-http-status');
 
-router.get('/', function (request, response) {
-    response.status(HttpStatus['OK']).json({cost: 64});
+router.get('/:itemName/:attackItem', function (request, response) {
+    response.status(HttpStatus['OK']).json({
+        item: request.params.itemName,
+        attackItem: request.params.attackItem,
+        cost: 64
+    });
 });
 
 module.exports = router;
--- features/rustItems.feature
+++ features/rustItems.feature
@@ -4,6 +4,6 @@ Feature: Rust Items
   So that I can return Item Information
 
   Scenario: Return amount of explosive ammo to destroy sheet metal door
-    When I go to the /durability endpoint
+    When I go to the /durability/sheetmetaldoor/explosivebullets endpoint
     Then it should return OK
     And the body should contain the cost
