503d95e79b46da964f0db162a5ddc5b832461a01
Sam Greaves <samgreaves0@gmail.com>
2019-02-01 20:58:06 +0000

    added more echo for printing in circle ci

--- tests.sh
+++ tests.sh
@@ -4,16 +4,21 @@ exitcode=0
 
 echo "Entered the tests.sh script"
 
+echo "Docker compose up"
 docker-compose --project-name rustrestapitest up -d
 
 sleep 5
 
+echo "Docker compose exitcode"
 docker-compose --project-name rustrestapitest up --exit-code-from rust-rest-api rust-rest-api
 exitcode=$?
 
+echo "Docker Stop"
 docker-compose --project-name rustrestapitest stop
+echo "Docker Remove"
 docker-compose --project-name rustrestapitest rm -f
 
+echo "Docker volume prune"
 docker volume prune -f
 
 if [[ ${exitcode} > 0 ]]; then
